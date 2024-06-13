2743df2cc06b7950773f2d4dab9f2991cc44318e
pedrohcferri <pedrohcferrri@gmail.com>
2024-06-13 13:59:01 -0300

    comitnovo

--- app.js
+++ app.js
@@ -1,31 +1,65 @@
+const banner = document.querySelector('.img_imc');   
+
+
+document.getElementById('meuForm').addEventListener('submit', function(event) {
+    var campoValor = document.getElementById('campo').value;
+    
+    if (campoValor.trim() === '') {
+        alert('Por favor, preencha o campo!');
+        event.preventDefault(); // Impede o envio do formulário
+        }
+        });
+
 function calcularIMC(event){
-
-    event.preventDefault();
-
+    
+            
+            event.preventDefault();
     let alturaSelect = parseFloat(document.getElementById('altura').value);
     let pesoSelect = parseInt(document.getElementById('peso').value);
-
     let imc=pesoSelect/(alturaSelect*alturaSelect);
-    // alert("Seu IMC é: " + imc.toFixed(2));
+    
 
-    // let resultado = document.getElementById('resultado');
-    // resultado.innerHTML=`<div class="campo_resultado" id="resultado"><label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} </label>
-    //  <img src="imc_15748_l.webp" alt="referência imc" width="50%" height="100%"></img></div>`;
-    
-    if(imc < 18.5){
-        
     let resultado = document.getElementById('resultado');
-         resultado.innerHTML=`<div class="campo_resultado" id="resultado"><label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} <br>Você está a baixo do peso!</label><img src="hulk.jpeg" alt="referência imc" width="30%" height="100%"></img></div>`;
+    let mensagem, imagem;
+
     
-    } else if(imc > 18.5 && imc <24.9){
-        let resultado = document.getElementById('resultado');
-        resultado.innerHTML=`<div class="campo_resultado" id="resultado"><label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} <br>Você está com o peso na média!</label><img src="mclove.jpg" alt="referência imc" width="40%" height="100%"></img></div>`;
-    } else if(imc >= 25 && imc < 29.9){
-        let resultado = document.getElementById('resultado');
-        resultado.innerHTML=`<div class="campo_resultado" id="resultado"><label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} <br>Você está com sobrepeso!</label><img src="thor.png" alt="referência imc" width="30%" height="100%"></img></div>`;
-    } else if(imc >= 30){
-        let resultado = document.getElementById('resultado');
-        resultado.innerHTML=`<div class="campo_resultado" id="resultado"><label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} <br>Você está acima do peso!</label><img src="gordin.jpg" alt="referência imc" width="30%" height="100%"></img></div>`;
+    switch(true){
+        case imc < 18.5:
+            mensagem = "Você está abaixo do peso!";
+            imagem = "hulk.jpeg";
+            break;
+            case imc < 24.9:
+                mensagem = "Você está com o peso na média!";
+                imagem = "mclove.jpg";
+                break;
+                case imc < 29.9:
+                    mensagem = "Você está com sobrepeso!";
+                    imagem = "thor.png";
+                    break;
+                    case imc >= 30:
+                        mensagem = "Você está acima do peso!";
+            imagem = "gordin.jpg";
+
+            
     }
-   
-}
+ resultado.innerHTML = `
+ <div class="campo_resultado" id="resultado">
+ <label class="campo_texto">Seu IMC é: ${imc.toFixed(2)} <br>${mensagem}</label>
+ <img src="${imagem}" alt="referência imc" width="35%" height="100%">
+ </div>`;
+ 
+ }
+ 
+ // if (imc < 18.5) {
+ //     mensagem = "Você está abaixo do peso!";
+ //     imagem = "hulk.jpeg";
+ // } else if (imc < 24.9) {
+ //     mensagem = "Você está com o peso na média!";
+ //     imagem = "mclove.jpg";
+ // } else if (imc < 29.9) {
+ //     mensagem = "Você está com sobrepeso!";
+ //     imagem = "thor.png";
+ // } else {
+ //     mensagem = "Você está acima do peso!";
+ //     imagem = "gordin.jpg";
+ // }
